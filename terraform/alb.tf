28c1747628fa486f3a44a7fa2c15fe7175f3b96d
IrezD <dennysben@gmail.com>
2023-12-28 13:44:07 +0100

    Fix: Allowing  Port 443 to load balaner

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -6,7 +6,7 @@ resource "aws_lb" "alb" {
   subnets            = var.subnets_for_ecs
 }
 
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "listerner_80" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
@@ -23,4 +23,28 @@ resource "aws_lb_target_group" "target_group_P-5000" {
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.vpc_id
+}
+
+resource "aws_lb_listener" "listerner_443" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group_P-5001.arn
+  }
+}
+
+resource "aws_lb_target_group" "target_group_P-5001" {
+  name        = "alb-target-group-${var.env}"
+  port        = 5000
+  protocol    = "HTTPS"
+  target_type = "ip"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_lb_listener_certificate" "example" {
+  listener_arn    = aws_lb_listener.listerner_443.arn
+  certificate_arn = "arn:aws:acm:us-east-1:806066816337:certificate/3848def8-0982-4f7e-b2be-664c6d00d101"
 }
--- terraform/networking.tf
+++ terraform/networking.tf
@@ -11,6 +11,15 @@ resource "aws_security_group" "internet_to_ALB" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+   ingress {
+    description      = "Inbound from Internet to ALB"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
