b9a12c2ec3424e16c1f1f86084cbb1b22f337ad7
IrezD <dennysben@gmail.com>
2023-12-28 02:39:54 +0100

    Chores: Using unique resource name for both environments

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"Hello World!": "Merry Christmas to everyone and a happy new year!!!"}
+    return {"Hello World!": "Merry Christmas and a happy new year to everyone!!!"}
 
 
 @app.get("/items/{item_id}")
