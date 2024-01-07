9346e44ecc47e6fb55219bfd54062674ef90ef73
IrezD <dennysben@gmail.com>
2024-01-07 16:23:19 +0100

    removing security group from launch template

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,61 +1,61 @@
-# ------ ECS Cluster ------- **
+# # ------ ECS Cluster ------- **
 
-resource "aws_ecs_cluster" "fastapi-cluster" {
-  name = "fastapi-cluster"
+# resource "aws_ecs_cluster" "fastapi-cluster" {
+#   name = "fastapi-cluster"
 
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
+#   setting {
+#     name  = "containerInsights"
+#     value = "enabled"
+#   }
 
-  configuration {
-    execute_command_configuration {
-      logging = "DEFAULT"
-    }
-  }
-}
+#   configuration {
+#     execute_command_configuration {
+#       logging = "DEFAULT"
+#     }
+#   }
+# }
 
-# ------ Task definition ------- ** 
+# # ------ Task definition ------- ** 
 
-resource "aws_ecs_task_definition" "task_definition" {
-  family                   = "${var.env}-fastapi-container"
-  requires_compatibilities = ["EC2"]
-  container_definitions    = <<TASK_DEFINITION
-[
-  {
-    "name": "${var.env}_FastAPI_image",
-    "image": "${aws_ecrpublic_repository.fastapi-ecr-public.repository_uri}:${var.image_tag}",
-    "cpu": 256,
-    "memory": 512,
-    "essential": true,
-    "portMappings": [
-      {
-        "containerPort": 5000,
-        "hostPort": 5000
-      }
-    ]
-  }
-]
-TASK_DEFINITION
+# resource "aws_ecs_task_definition" "task_definition" {
+#   family                   = "${var.env}-fastapi-container"
+#   requires_compatibilities = ["EC2"]
+#   container_definitions    = <<TASK_DEFINITION
+# [
+#   {
+#     "name": "${var.env}_FastAPI_image",
+#     "image": "${aws_ecrpublic_repository.fastapi-ecr-public.repository_uri}:${var.image_tag}",
+#     "cpu": 256,
+#     "memory": 512,
+#     "essential": true,
+#     "portMappings": [
+#       {
+#         "containerPort": 5000,
+#         "hostPort": 5000
+#       }
+#     ]
+#   }
+# ]
+# TASK_DEFINITION
 
-}
+# }
 
-# ------ ECS Service -------- **
+# # ------ ECS Service -------- **
 
-resource "aws_ecs_service" "fastapi-service" {
-  name                 = var.ecs_service_name
-  cluster              = aws_ecs_cluster.fastapi-cluster.id
-  task_definition      = aws_ecs_task_definition.task_definition.arn
-  desired_count        = 1
-  launch_type          = "EC2"
-  force_new_deployment = true
+# resource "aws_ecs_service" "fastapi-service" {
+#   name                 = var.ecs_service_name
+#   cluster              = aws_ecs_cluster.fastapi-cluster.id
+#   task_definition      = aws_ecs_task_definition.task_definition.arn
+#   desired_count        = 1
+#   launch_type          = "EC2"
+#   force_new_deployment = true
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.target_group.arn
-    container_name   = "${var.env}_FastAPI_image"
-    container_port   = 5000
-  }
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.target_group.arn
+#     container_name   = "${var.env}_FastAPI_image"
+#     container_port   = 5000
+#   }
 
-}
+# }
 
 
