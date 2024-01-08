b4e46b0ca20b58f36bee4099758b83e1fed0141d
IrezD <dennysben@gmail.com>
2024-01-08 09:11:36 +0100

    Chores: unique name for instance profile in both environments

--- terraform/ecs_capacity_provider.tf
+++ terraform/ecs_capacity_provider.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_capacity_provider" "customProvider" {
-  name = "CustomEC2Provider"
+  name = var.capacity_provider_name
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.ASG_config.arn
@@ -15,12 +15,12 @@ resource "aws_ecs_capacity_provider" "customProvider" {
 resource "aws_ecs_cluster_capacity_providers" "Provider_config" {
   cluster_name = aws_ecs_cluster.fastapi-cluster.name
 
-  capacity_providers = ["CustomEC2Provider"]
+  capacity_providers = [var.capacity_provider_name]
 
   default_capacity_provider_strategy {
     base              = 1
     weight            = 100
-    capacity_provider = "CustomEC2Provider"
+    capacity_provider = var.capacity_provider_name
   }
 }
 
